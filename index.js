22744c548e094b3197c7a6e7a7e39e276015443b
SyedaIlmaAli <160448031+SyedaIlmaAli@users.noreply.github.com>
2024-05-27 03:10:47 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+//Day 02
+//Question 04
+console.log(`George Herbert once said: "When there is a will there is a will there is way"`);
+// Question 05
+let author = "George Herbert";
+let quote = "When there is a will there is a will there is way";
+console.log(`${author} once said: "${quote}"`);
+//Question 06
+let firstName = "\t\nIlma\t\n";
+console.log(firstName.trim());
---
+++ index.ts
@@ -0,0 +1,16 @@
+//Day 02
+
+//Question 04
+
+console.log(`George Herbert once said: "When there is a will there is a will there is way"`);
+
+// Question 05
+
+let author : string = "George Herbert";
+let quote : string = "When there is a will there is a will there is way";
+console.log(`${author} once said: "${quote}"`);
+
+//Question 06
+
+let firstName : string = "\t\nIlma\t\n"
+console.log(firstName.trim());
